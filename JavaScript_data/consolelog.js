4ced3fd03111f2332d4c0f85ae89b0cdc41affab
Sudip patra <sudippatra1311@gmail.com>
2026-07-23 21:26:17 +0530

    Add files via upload

---
+++ JavaScript_data/consolelog.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "sudip";
+const botLocation = "mumbai";
+const favoriteLanguage = "Hindi";
+
+console.log("My name is " + botName + " and I live on " + botLocation+ ".");
+console.log("My favorite language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " is the National language of india.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is also 1000 yrs old language.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is spoken by Indians in India.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
