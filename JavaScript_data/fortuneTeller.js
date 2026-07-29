289c6665fe9485cc91e05b8f58c1a83fb86d688d
Sudip patra <sudippatra1311@gmail.com>
2026-07-29 20:09:19 +0530

    Simple Fortune Teller

---
+++ JavaScript_data/fortuneTeller.js
@@ -0,0 +1,28 @@
+const fortune1 = "Your cat will look very cuddly today.";
+const fortune2 = "The weather will be nice tomorrow.";
+const fortune3 = "Be cautious of your new neighbors.";
+const fortune4 = "You will find a new hobby soon.";
+const fortune5 = "It would be wise to avoid the color red today.";
+
+const min = 1;
+const max = 5;
+const randomNumber =  Math.floor(Math.random() * (max - min + 1)) + min;
+
+let selectedFortune;
+if (randomNumber === 1) {
+  selectedFortune = fortune1;
+} 
+else if (randomNumber === 2) {
+  selectedFortune = fortune2;
+}
+else if (randomNumber === 3) {
+  selectedFortune = fortune3;
+}
+else if (randomNumber === 4) {
+  selectedFortune = fortune4;
+}
+else {
+  selectedFortune = fortune5;
+}
+
+console.log(`your fortune today: ${selectedFortune.toUpperCase()}`);
